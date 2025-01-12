31bb4e27c223e60ebac6d699be106b07bf7c1bea
Marlon <marlon.da.silva996@gmail.com>
2025-01-12 18:28:24 -0300

    switch

--- JavaScript - Básico/projeto/script.js
+++ JavaScript - Básico/projeto/script.js
@@ -1,15 +1,22 @@
-// while -> enquanto 
-var x = 0
+// switch
 
-while (x < 10) {
-    document.write('<br> o valor de x é: ' + x)
-    // x = x + 1
-    x++
-}
+function pedir() {
+    var valor = prompt('digite de um valor de 1 a 4')
 
-// for = para
-var limite = 29
-
-for (a = 0; a <= limite; a++) {
-    document.write('<br> o valor de a é: ' + a)
+    switch(Number(valor)){
+        case 1:
+            alert('você escolheu água')
+            break
+        case 2:
+            alert('você escolheu suco')
+            break
+        case 3:
+            alert('você escolheu cerveja')
+            break
+        case 4:
+            alert('você chamou o garçom!')
+            break
+        default:
+            alert('escolha uma opção entre 1 a 4')
+    }
 }
