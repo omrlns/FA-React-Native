03f07280b7cc527c0b244ce18c61fb2e83141fcc
Marlon <marlon.da.silva996@gmail.com>
2025-01-12 10:51:35 -0300

    criando funções

---
+++ JavaScript - Básico/projeto/script.js
@@ -0,0 +1,23 @@
+
+var area = document.getElementById('area')
+
+function entrar(){
+    var nome = prompt('digite o seu nome')
+
+    if (nome === '' || nome === null) {
+        alert('ops, algo deu errado :/')
+        area.innerHTML = 'clique no botão para acessar...'
+    } else {
+        area.innerHTML = 'bem-vindo, ' + nome + ' '
+
+        let botaoSair = document.createElement('button')
+        botaoSair.innerText = 'sair da conta'
+        botaoSair.onclick = sair
+        area.appendChild(botaoSair)
+    }
+}
+
+function sair() {
+    alert('até mais!')
+    area.innerHTML = 'você saiu!'
+}
