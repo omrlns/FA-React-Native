8f66b11e3126ec605140c7206ac393ee805e8106
Marlon <marlon.da.silva996@gmail.com>
2025-01-12 18:03:43 -0300

    aprendendo loops

--- JavaScript - Básico/projeto/script.js
+++ JavaScript - Básico/projeto/script.js
@@ -1,38 +1,15 @@
+// while -> enquanto 
+var x = 0
 
-var area = document.getElementById('area')
-
-function entrar(){
-    var nome = prompt('digite o seu nome')
-
-    if (nome === '' || nome === null) {
-        alert('ops, algo deu errado :/')
-        area.innerHTML = 'clique no botão para acessar...'
-    } else {
-        area.innerHTML = 'bem-vindo, ' + nome + ' '
-
-        let botaoSair = document.createElement('button')
-        botaoSair.innerText = 'sair da conta'
-        botaoSair.onclick = sair
-        area.appendChild(botaoSair)
-    }
+while (x < 10) {
+    document.write('<br> o valor de x é: ' + x)
+    // x = x + 1
+    x++
 }
 
-function sair() {
-    alert('até mais!')
-    area.innerHTML = 'você saiu!'
-}
+// for = para
+var limite = 29
 
-function mediaAluno(nota1, nota2) {
-    var media = (nota1 + nota2) / 2
-
-    if (media >= 7) {
-        console.log('aluno aprovado com a média: ' + media)
-    } else if (media < 7) {
-     console.log('aluno reprovado com a média: ' + media)
-    }
-}
-
-function aluno(nome, curso) {
-    var mensangem = 'seja bem-vindo ' + nome + ' ao curso de ' + curso
-    console.log(mensangem)
+for (a = 0; a <= limite; a++) {
+    document.write('<br> o valor de a é: ' + a)
 }
