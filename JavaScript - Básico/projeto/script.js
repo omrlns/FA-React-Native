a214df293718af1c85cef245f267e4185889118b
Marlon <marlon.da.silva996@gmail.com>
2025-01-19 10:19:40 -0300

    praticando conhecimentos com a calculadora de IMC

--- JavaScript - Básico/projeto/script.js
+++ JavaScript - Básico/projeto/script.js
@@ -1,22 +1,37 @@
-// switch
+// abaixo de 17 = muito abaixo do peso
+// entre 17 e 18,49 = abaixo do peso
+// entre 18,5 e 24,99 = peso normal
+// entre 25 e 29,99 = acima do peso
 
-function pedir() {
-    var valor = prompt('digite de um valor de 1 a 4')
+// peso / (altura*altura)
 
-    switch(Number(valor)){
-        case 1:
-            alert('você escolheu água')
-            break
-        case 2:
-            alert('você escolheu suco')
-            break
-        case 3:
-            alert('você escolheu cerveja')
-            break
-        case 4:
-            alert('você chamou o garçom!')
-            break
-        default:
-            alert('escolha uma opção entre 1 a 4')
+var peso, altura, imc, resultado
+
+function calcular(event) {
+    event.preventDefault()
+
+    peso = document.getElementById('peso').value
+    altura = document.getElementById('altura').value
+
+    imc = peso / (altura * altura)
+
+    if (imc < 17) {
+        resultado = document.getElementById('resultado')
+        resultado.innerHTML = ' </br> seu resultado foi: ' + imc.toFixed(2) + ' </br> CUIDADO! você está muito abaixo do peso! cuide da sua saúde!'
+    } else if (imc > 17 && imc <= 18.49) {
+        resultado = document.getElementById('resultado')
+        resultado.innerHTML = ' </br> seu resultado foi: ' + imc.toFixed(2) + ' </br> você está abaixo do peso! procure um médico!'
+    } else if (imc >= 18.5 && imc < 24.99) {
+        resultado = document.getElementById('resultado')
+        resultado.innerHTML = ' </br> seu resultado foi: ' + imc.toFixed(2) + ' </br> PARABÉNS! você está no seu peso ideal!'
+    } else if (imc > 25 && imc < 29.99) {
+        resultado = document.getElementById('resultado')
+        resultado.innerHTML = ' </br> seu resultado foi: ' + imc.toFixed(2) + ' </br> você está acima do peso, movimente-se!'
+    } else if (imc >= 30) {
+        resultado = document.getElementById('resultado')
+        resultado.innerHTML = ' </br> seu resultado foi: ' + imc.toFixed(2) + ' </br> CUIDADO OBESIDADE!'
     }
+
+
+
 }
