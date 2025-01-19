f9444ff2092102156c8273ce34eb26d209c55b57
Marlon <marlon.da.silva996@gmail.com>
2025-01-19 11:40:01 -0300

    temporizadores

---
+++ JavaScript - Avançado/Temporizadores/script.js
@@ -0,0 +1,16 @@
+// temporizadores
+
+// setInterval
+function acao() {
+    document.write('teste! <br/>')
+}
+
+// setInterval(acao, 1000)
+
+// função anônima
+// var timer = setInterval(() => {
+//     document.write('executando! <br/>')
+// }, 1000)
+
+// setTimeout
+setTimeout(acao, 3000)
