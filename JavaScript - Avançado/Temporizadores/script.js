93a20de6b50ba0082123fa3a67642fda7c0d0ffb
omrlns <marlon.da.silva996@gmail.com>
2025-03-02 14:19:18 -0300

    praticando com uma lista de tarefas

--- JavaScript - Avançado/Temporizadores/script.js
+++ JavaScript - Avançado/Temporizadores/script.js
@@ -1,10 +1,55 @@
-// template strings
+let listElement = document.querySelector('#app ul')
+let inputElement = document.querySelector('#app input')
+let buttonElement = document.querySelector('#app button')
 
-let nome = 'Marlon'
-let sobrenome = 'Silva'
-let idade = 26
+let tarefas = JSON.parse(localStorage.getItem('@listaDeTarefas')) || []
 
-let mensagem1 = 'Meu nome é ' + nome + ' ' + sobrenome + ' e eu tenho ' + idade + ' anos de idade'
-let mensagem2 = `Meu nome é ${nome} ${sobrenome} e eu tenho ${idade} anos de idade`
+function adicionarTarefas() {
+    if (inputElement.value === '') {
+        alert('DIGITE ALGUMA TAREFA')
+        return false
+    } else {
+        let novaTarefa = inputElement.value
+        tarefas.push(novaTarefa)
+        inputElement.value = ''
+        renderTarefas()
+        salvarDados()
+    }
+}
 
-console.log(mensagem2)
+function renderTarefas() {
+    listElement.innerHTML = ''
+    tarefas.map((toDo) => {
+        let liElement = document.createElement('li')
+        let tarefaText = document.createTextNode(toDo)
+
+        let linkElement = document.createElement('a')
+        linkElement.setAttribute('href', '#')
+
+        let linkText = document.createTextNode(' excluir')
+        linkElement.appendChild(linkText)
+
+        let posicao = tarefas.indexOf(toDo)
+
+        linkElement.setAttribute('onclick', `deletarTarefa(${posicao})`)
+
+        liElement.appendChild(tarefaText)
+        liElement.appendChild(linkElement)
+        listElement.appendChild(liElement)
+
+    })
+}
+
+function deletarTarefa(posicao) {
+    tarefas.splice(posicao, 1)
+    renderTarefas()
+    salvarDados()
+}
+
+function salvarDados() {
+    localStorage.setItem('@listaDeTarefas', JSON.stringify(tarefas))
+}
+
+buttonElement.onclick = adicionarTarefas
+
+renderTarefas()
